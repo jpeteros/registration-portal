ed2764d4846d102acc788364a335c02fad2c6885
unknown <rickeeper@gmail.com>
2020-05-31 21:41:55 +0800

    added apollo resolver

--- ui-web/src/index.js
+++ ui-web/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from "@apollo/react-hooks";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { client } from "./apollo";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
